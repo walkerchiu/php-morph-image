11b38ee3378352b0cdcf1eb4adbc21d5c00c733c
walkerchiu <chenjen.chiou@gmail.com>
2021-05-04 14:22:12 +0800

    Feat (Function): Update list function

--- src/Models/Repositories/ImageRepository.php
+++ src/Models/Repositories/ImageRepository.php
@@ -113,9 +113,15 @@ class ImageRepository extends Repository
                                                 $query->ofEnabled()
                                                       ->whereIn('id', $data['tags']);
                                             });
+                                        })
+                                        ->unless(!empty($data['orderBy']) && !empty($data['orderType']), function ($query) use ($data) {
+                                            return $query->orderBy($data['orderBy'], $data['orderType']);
+                                        }, function ($query) {
+                                            return $query->orderBy('updated_at', 'DESC');
                                         });
+                            }, function ($query) {
+                                return $query->orderBy('updated_at', 'DESC');
                             })
-                            ->orderBy('updated_at', 'DESC')
                             ->get()
                             ->when(is_integer($page) && is_integer($nums), function ($query) use ($page, $nums) {
                                 return $query->forPage($page, $nums);
